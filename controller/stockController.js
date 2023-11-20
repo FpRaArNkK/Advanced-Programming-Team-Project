8238abad7dc7fb2108a80d65428dc2ce236d6d61
FpRaArNkK <mspark1184@naver.com>
2023-11-20 21:24:13 +0900

    [Feat] postStocks/weights func build

--- controller/stockController.js
+++ controller/stockController.js
@@ -42,34 +42,56 @@ module.exports = {
 
     post_stocks: async (req,res) => {
         const post = req.body;
-        const stock_names = post.names; // 이거 안되면 바로 req.body로 받아보기
-
-        console.log(stock_names);
-        
-        res.status(200).json(response(baseResponse.SUCCESS, stock_names));
-
-        // let input_array = [];
-
-        // stock_names.forEach(item => {
-        //     const selected_stock = { stock: item, weight: 0 };
-        //     input_array.push(selected_stock);
-        // });
-
-        // try {
-        //     const query = { selected_stocks: input_array };
-        //     const result = await  User.findByIdAndUpdate(req.session.user_id, query, {new : true}); // id 에 해당하는 doc 의 정보를 query 의 내용대로 수정
-        //     req.session.selected_stocks = input_array; 
-        //     res.status(200).json(response(baseResponse.SUCCESS, result));
-        // } catch (err) {
-        //     console.log(err);
-        //     res.status(500).json(response(baseResponse.SERVER_ERROR, error.message));
-        // }
+        const names = post.names;
+        const trimmed = names.replace(/'/g, ''); // 따옴표 제거
+        const stock_array = trimmed.slice(1, -1).split(',');
 
 
+        let input_array = [];
+
+        stock_array.forEach(item => {
+            const selected_stock = { stock: item, weight: 0 };
+            input_array.push(selected_stock);
+        });
+
+        try {
+            const query = { selected_stocks: input_array };
+            const result = await  User.findByIdAndUpdate(req.session.user_id, query, {new : true}); // id 에 해당하는 doc 의 정보를 query 의 내용대로 수정
+            req.session.selected_stocks = input_array; 
+            res.status(200).json(response(baseResponse.SUCCESS, result));
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(response(baseResponse.SERVER_ERROR, error.message));
+        }
     },
 
     post_weights: async (req,res) => {
+        const post = req.body;
 
+        const names = post.names;
+        const trimmed = names.replace(/'/g, ''); // 따옴표 제거
+        const stock_array = trimmed.slice(1, -1).split(',');
+
+        const weights = post.weights;
+        const weight_array = weights.slice(1, -1).split(',');
+
+
+        let input_array = [];
+
+        for (let i = 0; i < stock_array.length; i++) {
+            const selected_stock = { stock: stock_array[i], weight: weight_array[i] };
+            input_array.push(selected_stock);
+        }
+
+        try {
+            const query = { selected_stocks: input_array };
+            const result = await  User.findByIdAndUpdate(req.session.user_id, query, {new : true}); // id 에 해당하는 doc 의 정보를 query 의 내용대로 수정
+            req.session.selected_stocks = input_array; 
+            res.status(200).json(response(baseResponse.SUCCESS, result));
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(response(baseResponse.SERVER_ERROR, error.message));
+        }
     }
 }
 
